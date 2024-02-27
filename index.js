4112d4e1595990b0fc9ad9565e70e4f48dc4a98b
ellyzaveta <lizamari21@gmail.com>
2024-02-27 17:25:40 -0500

    modified:   index.js

--- index.js
+++ index.js
@@ -24,27 +24,27 @@ try {
     console.log(`\nTest addition of int numbers: ${int1}, ${int2}`);
     let intSum = addon.add(int1, int2);
     assert.strictEqual(intSum, int1 + int2, `${int1} + ${int2} should equal ${int1 + int2}`);
-    console.log(`🟢 Result ${intSum} - Passed.\n`);
+    console.log(`+ Result ${intSum} - Passed.\n`);
 
     const num1 = getRandomFloat(1, 100);
     const num2 = getRandomFloat(1, 100);
     console.log(`Test addition of float numbers: ${num1}, ${num2}`);
     let floatSum = addon.add(num1, num2);
     assert.strictEqual(floatSum, num1 + num2, `${num1} + ${num2} should equal ${num1 + num2}`);
-    console.log(`🟢 Result ${floatSum} - Passed.\n`);
+    console.log(`+ Result ${floatSum} - Passed.\n`);
 
     const str1 = getRandomString(5);
     const str2 = getRandomString(5);
     console.log(`Test concatenation of strings: "${str1}", "${str2}"`);
     let concatenatedString = addon.add(str1, str2);
     assert.strictEqual(concatenatedString, str1 + str2, `"${str1}" + "${str2}" should equal "${str1 + str2}"`);
-    console.log(`🟢 Result ${concatenatedString} - Passed.\n`);
+    console.log(`+ Result ${concatenatedString} - Passed.\n`);
 
     console.log(`Test concatenation of string and number: "${str1}", "${int1}"`);
     concatenatedString = addon.add(str1, int1);
     assert.strictEqual(concatenatedString, str1 + int1, `"${str1}" + "${int1}" should equal "${str1 + int1}"`);
-    console.log(`🟢 Result ${concatenatedString} - Passed.\n`);
+    console.log(`+ Result ${concatenatedString} - Passed.\n`);
 
 } catch (error) {
-    console.error('🔴 Test failed:', error.message);
+    console.error('- Test failed:', error.message);
 }
